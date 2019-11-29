b4e2a791e1f69feeb14fb3f99cfdaec72f9434e8
pegasis <admin@pegasis.site>
2019-11-29 03:23:42 -0500

    + fixed use V4 serializer to serialize V5 data

--- src/modes/server/serializers/SerializersCollection.kt
+++ src/modes/server/serializers/SerializersCollection.kt
@@ -7,6 +7,7 @@ import org.json.simple.JSONArray
 import org.json.simple.JSONObject
 import modes.server.serializers.CourseListPublicSerializer.Companion.serializeCourseList as serializePublicCourseList
 import modes.server.serializers.CourseListSerializerV4.serializeCourseList as serializeCourseListV4
+import modes.server.serializers.CourseListSerializerV5.serializeCourseList as serializeCourseListV5
 import modes.server.serializers.TimeLineSerializerV4.serializeTimeLine as serializeTimeLineV4
 import modes.server.serializers.TimeLineSerializerV5.serializeTimeLine as serializeTimeLineV5
 
@@ -19,7 +20,7 @@ fun JSONArray.wrapVersion(version: Int): JSONObject {
 
 val CourseListSerializers = mapOf<Int, (CourseList) -> JSONArray>(
     4 to ::serializeCourseListV4,
-    5 to ::serializeCourseListV4
+    5 to ::serializeCourseListV5
 )
 
 fun CourseList.serialize(version: Int = latestApiVersion): JSONObject {
