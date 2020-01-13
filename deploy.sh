80d8f3fcb0bd09ad1f669e06917b116ce16b7c1b
pegasis <admin@pegasis.site>
2020-01-13 00:40:36 -0500

    + update restart and deploy script to use 5007 control port

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 echo "Copying file....."
-sshfs pegasis@i.pegasis.site:/home/pegasis ~/sftp
-cp ./out/artifacts/fetch_ta_data_jar/fetch_ta_data.jar ~/sftp/yrdsb_ta_server/fetch_ta_data.jar.temp || exit
+rsync -z ./out/artifacts/fetch_ta_data_jar/fetch_ta_data.jar pegasis@i.pegasis.site:/home/pegasis/yrdsb_ta_server/fetch_ta_data.jar.temp || exit
 echo "File copied"
 
 ssh i.pegasis.site '
@@ -8,7 +7,7 @@ cd /home/pegasis/yrdsb_ta_server/ || exit
 screen -S ta-server -p 0 -X stuff "^C"
 sleep 5
 rm ./fetch_ta_data.jar
-mv ./fetch_ta_data.jar.temp ./fetch_ta_data.jar
-screen -S ta-server -d -m java -jar fetch_ta_data.jar server -p
+cp ./fetch_ta_data.jar.temp ./fetch_ta_data.jar
+screen -S ta-server -d -m java -jar fetch_ta_data.jar server -p --control-port 5007
 '
 echo "Server restarted"
