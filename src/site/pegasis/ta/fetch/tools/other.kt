299a0af1707e208e7b187a45f6711dae3f9b864e
pegasis <admin@pegasis.site>
2020-04-08 06:49:08 -0400

    - remove unused code

--- src/site/pegasis/ta/fetch/tools/other.kt
+++ src/site/pegasis/ta/fetch/tools/other.kt
@@ -6,8 +6,6 @@ import java.io.StringWriter
 
 val jsonParser = JSONParser()
 
-fun getCoreCount() = Runtime.getRuntime().availableProcessors()
-
 fun Throwable.toStackTrace(): String {
     val sw = StringWriter()
     val pw = PrintWriter(sw)
