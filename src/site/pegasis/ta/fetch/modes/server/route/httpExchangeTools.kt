6a5daa57b8d9bfa62d8fc64a40e485e103dd18a6
pegasis <admin@pegasis.site>
2020-04-08 09:21:08 -0400

    - Remove unused code

--- src/site/pegasis/ta/fetch/modes/server/route/httpExchangeTools.kt
+++ src/site/pegasis/ta/fetch/modes/server/route/httpExchangeTools.kt
@@ -1,6 +1,5 @@
 package site.pegasis.ta.fetch.modes.server.route
 
-import com.sun.net.httpserver.HttpExchange
 import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.http.HttpMethod
@@ -15,65 +14,7 @@ import io.ktor.util.pipeline.PipelineContext
 import site.pegasis.ta.fetch.modes.server.latestApiVersion
 import site.pegasis.ta.fetch.modes.server.minApiVersion
 import site.pegasis.ta.fetch.tools.gzip
-import java.nio.charset.StandardCharsets
 import kotlin.math.max
-
-fun HttpExchange.toHttpSession() = object : HttpSession {
-    override suspend fun getReqString() = String(
-        requestBody.readAllBytes(),
-        StandardCharsets.UTF_8
-    )
-
-    override fun getIP() = if (requestHeaders.containsKey("X-real-ip")) {
-        requestHeaders["X-real-ip"]?.get(0)
-    } else {
-        remoteAddress.address.toString()
-    }
-
-    override fun getApiVersion(): Int {
-        var apiVersion = 1
-        try {
-            apiVersion = requestHeaders["api-version"]!![0].toInt()
-            if (apiVersion > latestApiVersion) {
-                apiVersion = 1
-            }
-        } catch (e: Exception) {
-        }
-
-        return apiVersion
-    }
-
-    override fun isApiVersionInsufficient(minApi: Int): Boolean {
-        return getApiVersion() < Integer.max(minApi, minApiVersion)
-    }
-
-    override suspend fun send(status: Int, res: String, isGzip: Boolean) {
-        send(
-            status, if (res != "" && isGzip) {
-            res.gzip()
-        } else {
-            res.toByteArray()
-        }
-        )
-    }
-
-    override suspend fun send(statusCode: Int, body: ByteArray) {
-        sendResponseHeaders(statusCode, body.size.toLong())
-        responseBody.write(body)
-        responseBody.close()
-    }
-
-    override suspend fun makePublic(): Boolean {
-        responseHeaders.add("Access-Control-Allow-Origin", "*")
-        if (requestMethod.toUpperCase() == "OPTIONS") {
-            responseHeaders.add("Access-Control-Allow-Methods", "GET, OPTIONS")
-            responseHeaders.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
-            sendResponseHeaders(204, -1)
-            return true
-        }
-        return false
-    }
-}
 
 fun PipelineContext<Unit, ApplicationCall>.toHttpSession() = object : HttpSession {
     override suspend fun getReqString(): String {
