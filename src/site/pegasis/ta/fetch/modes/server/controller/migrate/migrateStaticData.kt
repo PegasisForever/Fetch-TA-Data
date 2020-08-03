99e71ae8e52de0843cc3c86d2d7d812b349032d8
pegasis <admin@pegasis.site>
2020-08-03 18:59:36 -0400

    fix migrate static data

--- src/site/pegasis/ta/fetch/modes/server/controller/migrate/migrateStaticData.kt
+++ src/site/pegasis/ta/fetch/modes/server/controller/migrate/migrateStaticData.kt
@@ -6,6 +6,7 @@ import org.json.simple.JSONArray
 import org.json.simple.parser.JSONParser
 import site.pegasis.ta.fetch.modes.server.storage.StaticData
 import site.pegasis.ta.fetch.tools.VALUE
+import site.pegasis.ta.fetch.tools.enableUpsert
 import site.pegasis.ta.fetch.tools.logInfo
 import java.io.File
 
@@ -15,10 +16,10 @@ suspend fun migrateStaticData(db: MongoDatabase) {
     val jsonParser = JSONParser()
 
     val announcement = File("data/announcement.txt").readText().trim()
-    collection.insertOne(Document("_id", StaticData.ANNOUNCEMENT_KEY).append(VALUE, announcement))
+    collection.updateOne(Document("_id", StaticData.ANNOUNCEMENT_KEY), Document("\$set", Document(VALUE, announcement)), enableUpsert)
 
     val calendar = jsonParser.parse(File("data/calendar.json").readText()) as JSONArray
-    collection.insertOne(Document("_id", StaticData.CALENDAR_KEY).append(VALUE, calendar))
+    collection.updateOne(Document("_id", StaticData.CALENDAR_KEY), Document("\$set", Document(VALUE, calendar)), enableUpsert)
 
     logInfo("Migrated static data.")
 }
