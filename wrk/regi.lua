85b7ade88951bf1c504749102e43661f9b351a0b
pegasis <admin@pegasis.site>
2021-01-03 16:40:26 -0500

    add wrk benchmark script for /regi

---
+++ wrk/regi.lua
@@ -0,0 +1,3 @@
+wrk.method = "POST"
+wrk.body = [[{"user":{"number":"349891234","password":"password","receive":false,"displayname":""},"language":"en","token":null}]]
+wrk.headers["api-version"] = "12"
