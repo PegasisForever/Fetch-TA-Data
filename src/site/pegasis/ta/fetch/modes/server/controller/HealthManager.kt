87320b0bc933870d73560cfc706199b284d26739
pegasis <admin@pegasis.site>
2021-01-04 07:14:55 -0500

    health manager only remove record if count > maxRecordCount and the record is over 5 mins old

--- src/site/pegasis/ta/fetch/modes/server/controller/HealthManager.kt
+++ src/site/pegasis/ta/fetch/modes/server/controller/HealthManager.kt
@@ -12,11 +12,17 @@ import java.util.concurrent.Callable
     version = ["BN$serverBuildNumber"]
 )
 class HealthManager(private val controllerResponse: ControllerResponse) : Callable<Unit> {
+    private data class Record(val statusCode: Int) {
+        val time = System.currentTimeMillis()
+
+        fun isError() = statusCode >= 500
+    }
+
     override fun call() {
         with(controllerResponse) {
             var hasError = false
-            records.forEach { (path, record) ->
-                val errorCount = record.count { it >= 500 }
+            recordsMap.forEach { (path, record) ->
+                val errorCount = record.count { it.isError() }
                 if (errorCount > 0) {
                     hasError = true
                     writeErrLine("$path: error $errorCount/${record.size}")
@@ -30,21 +36,22 @@ class HealthManager(private val controllerResponse: ControllerResponse) : Callab
     }
 
     companion object {
-        private val records = hashMapOf<String, Queue<Int>>()
+        private val recordsMap = hashMapOf<String, Queue<Record>>()
         private const val maxRecordCount = 30
 
         fun addRecord(path: String, status: Int) {
-            val record = if (path in records.keys) {
-                records[path]!!
+            val records = if (path in recordsMap.keys) {
+                recordsMap[path]!!
             } else {
-                val newRecord = ArrayDeque<Int>()
-                records[path] = newRecord
+                val newRecord = ArrayDeque<Record>()
+                recordsMap[path] = newRecord
                 newRecord
             }
 
-            record.offer(status)
-            while (record.size > maxRecordCount) {
-                record.poll()
+            records.offer(Record(status))
+            val currTime = System.currentTimeMillis()
+            while (records.size > maxRecordCount && records.peek().time < currTime - 5 * 60 * 1000) {
+                records.poll()
             }
         }
     }
